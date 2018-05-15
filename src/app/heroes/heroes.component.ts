85dbc0c905d9b0bad8e524445fda458795709a7c
Paul Highum <phigh07@gmail.com>
2018-05-15 09:40:30 -0600

    finished tutorial displaying a list step

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,19 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero"
 import { HEROES } from "../mock-heroes"
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero = {
+  hero: Hero = {
     id: 1,
     name: "Windstorm"
+  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
